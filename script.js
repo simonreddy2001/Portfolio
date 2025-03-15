362fb40fbd52bc3a8f2d5a9b731e3bde439805f7
simonreddy2001 <simon.reddy2001@gmail.com>
2025-03-15 09:29:43 +0100

    role changed

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["IT-Consultant", "FullStack-Developer", "Salesforce-Consultant", "Freelancer"],
+        strings: ["DevOps Cloud Engineer", "IT-Consultant", "FullStack-Developer", "Salesforce-Consultant", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["IT-Consultant", "FullStack-Developer", "Salesforce-Consultant", "Freelancer"],
+        strings: ["DevOps Cloud Engineer", "IT-Consultant", "FullStack-Developer", "Salesforce-Consultant", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
